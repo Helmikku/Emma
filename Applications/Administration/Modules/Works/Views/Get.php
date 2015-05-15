8cb2fd74576e72e96eb45a565550a733d749f7b3
Helmikku <tirikou@gmail.com>
2015-05-16 00:10:24 +0200

    Mise à jour des fichiers

--- Applications/Administration/Modules/Works/Views/Get.php
+++
@@ -1,5 +0,0 @@
-<?php if ($success) {
-	echo('{"success": true, "works": '.json_encode($works, JSON_NUMERIC_CHECK).'}');
-} else {
-	echo('{"success": false, "error": '.json_encode($error).'}');
-} ?>
